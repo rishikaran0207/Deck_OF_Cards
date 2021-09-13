1b72d3a82b1634abeaf5504e2fd1730976463223
rishikaran0207 <sendittomemrk@gmail.com>
2021-09-13 09:14:16 +0530

    Added: Creating a Deck of Cards

--- src/main/java/com/Biz/DeckofCards/DeckofCards.java
+++ src/main/java/com/Biz/DeckofCards/DeckofCards.java
@@ -2,5 +2,22 @@ package com.Biz.DeckofCards;
 public class DeckofCards {
     public static void main(String[] args) {
         System.out.println("Deck Of Cards");
+        String[] SUITS = {
+                "Clubs", "Diamonds", "Hearts", "Spades"
+        };
+
+        String[] RANKS = {
+                "2", "3", "4", "5", "6", "7", "8", "9", "10",
+                "Jack", "Queen", "King", "Ace"
+        };
+
+        // initialize deck
+        int n = SUITS.length * RANKS.length;
+        String[] deck = new String[n];
+        for (int i = 0; i < RANKS.length; i++) {
+            for (int j = 0; j < SUITS.length; j++) {
+                deck[SUITS.length*i + j] = RANKS[i] + " of " + SUITS[j];
+            }
+        }
     }
 }
